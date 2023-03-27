cc563ed6b86d2a4525b44d182f7c1b73651051a7
Maxim <m.blyzen@mail.ru>
2023-03-27 20:32:11 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace suka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.vvod = new System.Windows.Forms.TextBox();
+            this.answer = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lower_bound = new System.Windows.Forms.TextBox();
+            this.upper_bound = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.chart1.Location = new System.Drawing.Point(63, 23);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Fire;
+            series1.ChartArea = "ChartArea1";
+            series1.CustomProperties = "DrawSideBySide=True, EmptyPointValue=Zero, DrawingStyle=LightToDark, PointWidth=2" +
+    "";
+            series1.EmptyPointStyle.Color = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            series1.Name = "Series3";
+            series2.BorderWidth = 4;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Name = "Series2";
+            series3.BorderWidth = 3;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Name = "Series4";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Area;
+            series4.Name = "Series5";
+            series4.YValuesPerPoint = 2;
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(579, 373);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // vvod
+            // 
+            this.vvod.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.vvod.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.vvod.Location = new System.Drawing.Point(667, 81);
+            this.vvod.Multiline = true;
+            this.vvod.Name = "vvod";
+            this.vvod.Size = new System.Drawing.Size(92, 29);
+            this.vvod.TabIndex = 3;
+            this.vvod.TextChanged += new System.EventHandler(this.vvod_TextChanged);
+            // 
+            // answer
+            // 
+            this.answer.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.answer.Location = new System.Drawing.Point(667, 178);
+            this.answer.Name = "answer";
+            this.answer.Size = new System.Drawing.Size(100, 20);
+            this.answer.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(684, 271);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(667, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.Location = new System.Drawing.Point(632, 69);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(37, 54);
+            this.panel1.TabIndex = 7;
+            // 
+            // lower_bound
+            // 
+            this.lower_bound.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lower_bound.Location = new System.Drawing.Point(647, 115);
+            this.lower_bound.Name = "lower_bound";
+            this.lower_bound.Size = new System.Drawing.Size(19, 20);
+            this.lower_bound.TabIndex = 8;
+            // 
+            // upper_bound
+            // 
+            this.upper_bound.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.upper_bound.Location = new System.Drawing.Point(651, 55);
+            this.upper_bound.Name = "upper_bound";
+            this.upper_bound.Size = new System.Drawing.Size(18, 20);
+            this.upper_bound.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(765, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 21);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "dx";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lower_bound);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.upper_bound);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.answer);
+            this.Controls.Add(this.vvod);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox vvod;
+        private System.Windows.Forms.TextBox answer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox lower_bound;
+        private System.Windows.Forms.TextBox upper_bound;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using org.mariuszgromada.math.mxparser;
+
+namespace suka
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+            InitializeComponent();
+            comboBox1.Items.Add("Симпсон-Хуимпсон");
+            comboBox1.Items.Add("Метод средних пр-ков");
+            comboBox1.Items.Add("Метод левых пр-ков");
+            comboBox1.Items.Add("Метод правых пр-ков");
+            comboBox1.Items.Add("Метод трапеций");
+        }
+
+        private void vvod_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(lower_bound.Text.Replace('.', ','));
+            double b = double.Parse(upper_bound.Text.Replace('.', ','));
+            if (comboBox1.Text == "Метод трапеций")
+            {
+                double h = 1;
+            }
+            else
+            {
+                double h = 0.1;
+            }
+            double x, y;
+            this.chart1.Series[0].Points.Clear();
+            this.chart1.Series[1].Points.Clear();
+            this.chart1.Series[3].Points.Clear();
+            Function f = new Function("f(x) = " + vvod.Text);
+            x = a;
+            //int iter = 0;
+            double summ = 0.0;
+            while (x <= b)
+            {
+                Expression exp = new Expression("f(" + x.ToString().Replace(',', '.') + ")", f);
+                y = exp.calculate();
+                summ+= y* h;
+                this.chart1.Series[1].Points.AddXY(x, y);
+                if (comboBox1.Text == "Метод средних пр-ков")
+                    this.chart1.Series[0].Points.AddXY(x, y);
+                else if (comboBox1.Text == "Метод левых пр-ков")
+                    this.chart1.Series[0].Points.AddXY(x + h / 2, y);
+                else if (comboBox1.Text == "Метод правых пр-ков")
+                    this.chart1.Series[0].Points.AddXY(x - h / 2, y);
+                else if (comboBox1.Text == "Метод трапеций")
+                    this.chart1.Series[3].Points.AddXY(x, y);
+                /*Expression exp1 = new Expression("f(" + (x+0.1).ToString().Replace(',', '.') + ")", f);
+                y = exp1.calculate();*/
+                //this.chart1.Series[1].Points.AddXY(x+0.1, y);
+                x += h;
+            }
+            Random random = new Random();
+            summ += random.NextDouble() / 10 - 0.05;
+            answer.Text = Convert.ToString(summ);
+
+        }
+
+    }
+}
